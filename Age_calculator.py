7312ed517cc0f0e79fc47773e018f62a97a9e7c8
Krishna Kuila <krishnakuila722@gmail.com>
2025-10-17 21:45:34 +0530

    change in color

--- Age_calculator.py
+++ Age_calculator.py
@@ -13,6 +13,6 @@ if button and name and dob:
     month = int((age.days % 365)/30)
     day = (age.days % 365)%30
     st.write(f'{name}, Your age is :')
-    st.badge(f'{yrs} years and {month} month')
+    st.badge(f'{yrs} years and {month} month', color='green')
 else:
-    st.write('Value missing...',)
+    st.badge('Value missing...',color='red')
