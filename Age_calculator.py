20e34ee365408182b22977e9f96af473a57a8d6a
Krishna Kuila <krishnakuila722@gmail.com>
2025-10-17 21:05:52 +0530

    dob to DOB

--- Age_calculator.py
+++ Age_calculator.py
@@ -4,7 +4,7 @@ import datetime
 
 st.title(" Age Calcualtor")
 name = st.text_input('Enter your name :')
-dob = st.date_input('Enter your DoB :',value=None, min_value=("2000-01-01"), max_value=datetime.datetime.today(), width=210, format='DD/MM/YYYY')
+dob = st.date_input('Enter your DOB :',value=None, min_value=("2000-01-01"), max_value=datetime.datetime.today(), width=210, format='DD/MM/YYYY')
 button = st.button('Calculate')
 if button and name and dob:
     today = datetime.date.today()
