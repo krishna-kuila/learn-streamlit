b3af138716dcabb553fa40fb984321fee4198eb3
Krishna Kuila <krishnakuila722@gmail.com>
2025-10-17 23:11:51 +0530

    change in the logic

--- Age_calculator.py
+++ Age_calculator.py
@@ -8,11 +8,14 @@ dob = st.date_input('Enter your DOB :',value=None, min_value=("2000-01-01"), max
 button = st.button('Calculate')
 if button and name and dob:
     today = datetime.date.today()
+    cur_year, birth_year = today.year, (dob.year-1)
+    no_of_lyear = round(((cur_year/4)-(cur_year/100)+(cur_year/400)) - ((birth_year/4)-(birth_year/100)+(birth_year/400)))
     age = today - dob
-    yrs = int((age.days)/365)
-    month = int((age.days % 365)/30)
-    day = (age.days % 365)%30
+    days = age.days - no_of_lyear
+    yrs = (days)//365
+    month = (days % 365)//30
+    day = (days%365)%30
     st.write(f'{name}, Your age is :')
     st.badge(f'{yrs} years and {month} month {day} days.', color='green')
-else:
+elif button:
     st.badge('Value missing...',color='red')
